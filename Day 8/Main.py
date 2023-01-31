0edd54a999dc726b966154cc5f05e020ca5971fc
Adarsh Singh <adarshsinghak001@gmail.com>
2023-01-31 23:39:29 +0530

    Day 8 in Python

---
+++ Day 8/Main.py
@@ -0,0 +1,26 @@
+# CALCULATOR
+print("CALCULATOR")
+num1 = int(input("Enter num1:- "))
+num2 = int(input("Enter num2:- "))
+
+print("////////////////////////////////")
+print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Exponential\n7.Floor Division")
+print("////////////////////////////////")
+
+operation = int(input("Choose a option for Calculation :- "))
+if(operation == 1):
+   print("your answer is:- ",num1+num2)
+elif(operation == 2):
+    print("your answer is:- ",num1-num2)
+elif(operation == 3):
+    print("your answer is:- ",num1*num2)  
+elif(operation == 4):
+    print("your answer is:- ",num1/num2)
+elif(operation == 5):
+    print("your answer is:- ",num1%num2)  
+elif(operation == 6):
+    print("your answer is:- ",num1**num2)
+elif(operation == 7):
+    print("your answer is:- ",num1//num2)        
+else:
+    print("Wrong Option Choosen")   

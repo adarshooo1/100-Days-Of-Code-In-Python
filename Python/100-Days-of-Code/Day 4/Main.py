b572b7239ef6a0220e7518965d76f37c25dd1d20
Adarsh Singh <98550817+adarshooo1@users.noreply.github.com>
2023-01-31 23:53:00 +0530

    Delete Python/100-Days-of-Code directory

--- Python/100-Days-of-Code/Day 4/Main.py
+++
@@ -1,8 +0,0 @@
-#Printing the String type of Object(OH! its a comment which is ignored by the python compiler,Hence not throwing error)
-print("Hello World")
-
-#Can tell the class and type of object
-print(type(5))
-
-# This can also perform maths operation
-print(5+5)

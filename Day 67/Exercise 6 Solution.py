793b282719950a089eadd9da4090f979154074a7
Adarsh Singh <adarshsinghak001@gmail.com>
2023-02-16 22:30:13 +0530

    Updated

--- Day 67/Exercise 6 Solution.py
+++ Day 67/Exercise 6 Solution.py
@@ -1,15 +1,17 @@
 # Library Management System
 
 class Library:
+    #This method will have the count of the books as well as lisy of books.
     def __init__(self):
         self.noOfBooks = 0
         self.books = []
 
+    #This will help to add the books.
     def addBook(self , book):
         self.books.append(book)
         self.noOfBooks = len(self.books)
 
-
+    #This method will show the details of book in the Library.
     def showBooks(self):
         print(f"The books in the Library are {self.books} books and total number of books is {self.noOfBooks}")    
 

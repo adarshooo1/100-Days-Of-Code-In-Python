6e34f36dca7db69d31df042119c957b15b209c01
Adarsh Singh <adarshsinghak001@gmail.com>
2023-03-09 23:46:54 +0530

    Day 97 Modified and changed In Python

--- Day 97/Main.py
+++ Day 97/Main.py
@@ -1,24 +1,39 @@
-# multiprocessing is a in-built module which helps in Multi-Threading.
-import multiprocessing
-import requests
-import time
+import threading # import threading module for multi-threading support
+import time # import time module to calculate time intervals
 
-def downloadFile(url , name):
-    response = requests.get(url)
-    with open(f"files/file{name}_{int(time.time())}.jpg" , "wb") as f:
-        f.write(response.content)
+# function that sleeps for the specified number of seconds
+def fun(seconds):
+    print(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds) # sleep for the specified number of seconds
 
-if __name__ == '__main__':
-    url = "https://picsum.photos/2000/3000"
+# normal sequential execution
+tim1 = time.perf_counter() # record the current time
+fun(4)    # call fun with a sleep time of 4 seconds
+fun(3)    # call fun with a sleep time of 3 seconds
+fun(10)   # call fun with a sleep time of 10 seconds
+print("ENDED")
+tim2 = time.perf_counter() # record the time after all three calls to fun have finished
 
-    pros = []
+print(tim2 - tim1) # calculate and print the total execution time
 
-    for i in range(5):
-        # downloadFile(url , i)
-        download = multiprocessing.Process(target=downloadFile , args=[url , i])
-        download.start()
-        pros.append(download)
+# multi-threaded execution
+tim3 = time.perf_counter() # record the current time
 
-    for download in pros:
-        download.join()
+# create three threads, each running the fun function with a different sleep time
+t1 = threading.Thread(target=fun ,args=[4])
+t2 = threading.Thread(target=fun ,args=[10])
+t3 = threading.Thread(target=fun ,args=[2])
 
+# start the threads running
+t1.start()
+t2.start()
+t3.start()
+
+# wait for each thread to finish before continuing
+t1.join()
+t2.join()
+t3.join()
+
+tim4 = time.perf_counter() # record the time after all threads have finished
+
+print(tim4 - tim3) # calculate and print the total execution time of the threaded block
